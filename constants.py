d52b29bebc3ca92938d7edcde8c1d892363554a0
renyhp <renyhp@disroot.org>
2020-07-10 10:49:00 +0200

    Use running commit hash in version

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ LILYSETTINGS_PATH = "lilysettings.ly"
 USER_FILES_DIR = "user_files"
 
 VERSION_NUMBER = "2.1"
-COMMIT_HASH = subprocess.check_output(["git", "rev-parse", "--short", "origin/master"]).decode("utf-8")[:-1]
+COMMIT_HASH = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8")[:-1]
 LILY_VERSION = lilypond.lilypond_process(["-v"])[0].split('\n', 1)[0][13:]
 
 START_TIME = datetime.now(timezone.utc)
